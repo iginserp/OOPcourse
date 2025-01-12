f3ee8b983e436e5a7cca0ce5059475be9df0ab57
SIginashev <sergey.iginashev@gmail.com>
2025-01-12 21:17:53 +0330

    Initial commit

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
+class Product:
+    """Класс для представления продукта."""
+    name: str
+    description: str
+    price: float
+    quantity: int
+    def __init__(self, name, description, price, quantity):
+        """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра класса"""
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+
+
+class Category:
+    """Класс для представления категории."""
+    name: str
+    description: str
+    products: list
+
+    def __init__(self, name, description, products):
+        """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра класса"""
+        self.name = name
+        self.description = description
+        self.products = products
+
 if __name__ == "__main__":
     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
